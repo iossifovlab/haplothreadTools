d8bf21a310b1381bbd775b1e2349b775b5ccab58
Boris Yamrom <byamrom@pe2cl2-004.c.nygenome.org>
2020-07-13 11:13:54 -0400

    modifications related to processing NYGC chips B01 and B02

--- createPedMap.py
+++ createPedMap.py
@@ -4,18 +4,33 @@ from collections import defaultdict
 import pickle
 
 if len(sys.argv) < 3:
-    print("Usage: createPedMap.py <ped file> <fms.pckl file for SSC2380>")
+    print("Usage: createPedMap.py <ped file> <pedigree file>")
     exit(1)
 
 pedFn=sys.argv[1]
-fmsFn=sys.argv[2]
-md=defaultdict()
+#fmsFn=sys.argv[2]
+pedigreeFn = sys.argv[2]
+#md=defaultdict()
 H='fam indId dad mom gender role'.split(' ')
 
+"""
 F = open(fmsFn)
 families,badFamilies = pickle.load(F)
 F.close()
+"""
 
+#NYGCSampleID	sfid	spid	father	mother	sex	asd	role
+md = defaultdict()
+with open(pedigreeFn) as f:
+    hd = {v:i for i,v in enumerate(f.readline().strip("\n\r").split("\t"))}
+    for l in f:
+        cs = l.strip("\n\r").split("\t")
+        if "B01" in os.environ["PROJECT_DIR"] or "B02" in os.environ["PROJECT_DIR"]:
+            md[cs[hd["NYGCSampleID"]]] = cs
+        else:
+            md[cs[hd["spid"]]] = cs
+
+"""    
 for fd in list(families.values()):
     for pd in fd.memberInOrder:
         smId = os.path.basename(pd.atts['bamF']).split(".")[0]
@@ -31,19 +46,25 @@ for fd in list(families.values()):
 
 gMap = {'F':'2', 'M':'1'}
 aMap = {'prb':'2', 'sib':'1'}
-
+"""
+n = 0
 with open(pedFn, 'r') as f:
     # #N sampleId
     for l in f:
+        n += 1
         cs = l.strip('\n\r').split('\t')
         if cs[1] in md:
-            out=md[cs[1]]
-            famId = out['fam']
-            out['gender']=gMap[out['gender']]
-            out['affectStatus'] = '1' if out['role'] in ['mom','dad'] else aMap[out['role']]
-            print('\t'.join([out[k] for k in H[:-1] 
-                             + ['affectStatus']]+cs[6:]))
-    
+            out = []
+            rec = md[cs[1]]
+            out.append(rec[hd["sfid"]])
+            out.append(rec[hd["spid"]])
+            out.append(rec[hd["father"]])
+            out.append(rec[hd["mother"]])
+            out.append(rec[hd["sex"]])
+            out.append(rec[hd["asd"]])
+            print('\t'.join(out + cs[6:]))
+        #if n == 30:
+        #    break
 
  
  
