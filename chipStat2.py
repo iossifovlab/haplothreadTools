223b12c8c8d1eb98fa8d894bf7a09ccd9ab32210
Boris Yamrom <byamrom@pe2cl2-004.c.nygenome.org>
2020-07-13 10:58:57 -0400

    change related to transition to python3

--- chipStat2.py
+++ chipStat2.py
@@ -14,13 +14,13 @@ print(suffix, file=sys.stderr)
 fn = sorted(glob(suffix +"*"))
 print(fn, file=sys.stderr)
 F = open(fn[0], 'rb')
-stats = np.load(F)
+stats = np.load(F, allow_pickle=True)
 F.close()
 print('loaded', fn[0], file=sys.stderr)
 
 for f in fn[1:]:
     with open(f, 'rb') as F:
-        stats += np.load(F)
+        stats += np.load(F, allow_pickle=True)
 
 F=open('chip-statSummary.stat', 'wb')
 np.save(F, stats)
