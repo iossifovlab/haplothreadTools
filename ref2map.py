f53f9eb0cc36b6e29e9f2088e1d563bfaf90828d
Boris Yamrom <byamrom@pe2cl2-004.c.nygenome.org>
2020-07-13 11:21:10 -0400

    change related to the fact that chip chrX is 23

--- ref2map.py
+++ ref2map.py
@@ -12,11 +12,12 @@ refFn = sys.argv[2]
 
 GA = openRef(refFn)
 chrM = {'chr'+str(v):i+1 for i,v in enumerate(range(1,23))}
+chrM['chrX']=23
 
 with open(mapFn, 'r') as f:
     for l in f:
         cs = l.strip('\n\r').split(sep)
-        ch = 'chr'+cs[0]
+        ch = 'chr'+'X' if cs[0] == '23' else cs[0]
         p = cs[3]
         refA = GA.getSequence(ch, int(p),int(p)) if ch in chrM else ''
         print("\t".join(cs + [refA]))
